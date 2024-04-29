ed545d10db8211a34c75c68e565b7d6cb61fee4f
Daniel Velasquez <danivelas4@gmail.com>
2024-04-29 12:27:07 -0400

    .config/nvim/lua/keymaps.lua: added more keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -23,6 +23,15 @@ vim.keymap.set('v', 'd', '"_d', opts)
 vim.keymap.set('v', 'P', '"_dP', opts)
 vim.keymap.set('v', 'p', '"_dp', opts)
 
+-- readline bindings
+vim.keymap.set('i', '<C-A>', '<C-O>^')
+vim.keymap.set('c', '<C-A>', '<Home>')
+
+-- both insert and command mode
+vim.keymap.set('!', '<C-X><C-A>', '<C-A>')
+vim.keymap.set('!', '<C-B>', '<Left>')
+vim.keymap.set('!', '<C-D>', '<Delete>')
+
 -----------------
 -- Normal mode --
 -----------------
@@ -33,3 +42,17 @@ vim.keymap.set('n', '<leader>g', ':vertical rightbelow G<CR>', opts)
 -----------------
 -- Visual mode --
 -----------------
+
+-----------------
+-- Insert mode --
+-----------------
+-- shortcut to merge text of line with end of previous line
+vim.keymap.set('i', ',e', '<Esc>^"_d0i<Bs>', opts)
+
+
+-- i'd rather use c-d as a readline binding for deleting at point than shifting left
+-- so, i'll make c-t inherit the behavior of c-d
+-- if c-t is shift-left, then there's no way to shift right.
+-- i'l make c-y shift right since it's not being used for anything and it's to the right of c-t
+vim.keymap.set('i', '<C-t>', '<C-d>', opts)
+vim.keymap.set('i', '<C-y>', '<C-t>', opts)
