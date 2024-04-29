7f34f0c306af0de128256809ace22f95564c6425
Daniel Velasquez <danivelas4@gmail.com>
2024-04-29 13:59:12 -0400

    .config/nvim/lua/options.lua: added options from .vimrc

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,12 +1,11 @@
--- Hint: use `:h <option>` to figure out the meaning if needed
 vim.opt.clipboard = 'unnamedplus'   -- use system clipboard 
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
 
 -- Tab
-vim.opt.tabstop = 4                 -- number of visual spaces per TAB
-vim.opt.softtabstop = 4             -- number of spacesin tab when editing
-vim.opt.shiftwidth = 4              -- insert 4 spaces on a tab
+vim.opt.tabstop = 2                 -- number of visual spaces per TAB
+vim.opt.softtabstop = 2             -- number of spacesin tab when editing
+vim.opt.shiftwidth = 2              -- insert 4 spaces on a tab
 vim.opt.expandtab = true            -- tabs are spaces, mainly because of python
 
 -- UI config
@@ -15,11 +14,19 @@ vim.opt.relativenumber = true       -- add numbers to each line on the left side
 vim.opt.cursorline = true           -- highlight cursor line underneath the cursor horizontally
 vim.opt.splitbelow = true           -- open new vertical split bottom
 vim.opt.splitright = true           -- open new horizontal splits right
--- vim.opt.termguicolors = true        -- enabl 24-bit RGB color in the TUI
-vim.opt.showmode = false            -- we are experienced, wo don't need the "-- INSERT --" mode hint
+vim.opt.termguicolors = true        -- enable 24-bit RGB color in the TUI
 
 -- Searching
 vim.opt.incsearch = true            -- search as characters are entered
 vim.opt.hlsearch = false            -- do not highlight matches
 vim.opt.ignorecase = true           -- ignore case in searches by default
 vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
+
+vim.opt.showmode = true             -- show if we are in insert mode
+vim.opt.wrap = false                -- dont wrap lines if they exceed the length of the window
+vim.opt.sidescroll = 5              -- make side scrolling faster
+vim.opt.autoindent = true           -- copy indent from previous line to new line
+vim.opt.smartindent = true          -- Do smart autoindenting when starting a new line
+vim.opt.foldmethod = 'indent'       -- by default, fold code by indent
+vim.opt.showcmd = true
+vim.opt.hidden = true
