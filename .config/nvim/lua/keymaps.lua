7d05130c69215be7a7d62b9285b3f10b4b955585
Daniel Velasquez <danivelas4@gmail.com>
2024-04-29 11:49:30 -0400

    .config/nvim/lua/keymaps.lua: added config to not copy into unnamed register

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -4,19 +4,32 @@ local opts = {
     silent = true,       -- do not show message
 }
 
+-- Hint: see `:h vim.map.set()`
+
+-----------------
+--  All modes  --
+-----------------
+-- delete without writing to the unnamed register
+-- note: that with this config, the only key that deletes and writes to the unnamed register is x in visual mode
+vim.keymap.set('n', 'x', '"_x', opts)
+vim.keymap.set('n', 'X', '"_X', opts)
+vim.keymap.set('n', 'c', '"_c', opts)
+vim.keymap.set('n', 'C', '"_C', opts)
+vim.keymap.set('n', 'd', '"_d', opts)
+vim.keymap.set('n', 'D', '"_D', opts)
+
+vim.keymap.set('v', 'c', '"_c', opts)
+vim.keymap.set('v', 'd', '"_d', opts)
+vim.keymap.set('v', 'P', '"_dP', opts)
+vim.keymap.set('v', 'p', '"_dp', opts)
+
 -----------------
 -- Normal mode --
 -----------------
 
--- Hint: see `:h vim.map.set()`
-
--- Resize with arrows
+-- Open fugitive menu
 vim.keymap.set('n', '<leader>g', ':vertical rightbelow G<CR>', opts)
 
 -----------------
 -- Visual mode --
 -----------------
-
--- Hint: start visual mode with the same area as the previous area and the same mode
---vim.keymap.set('v', '<', '<gv', opts)
---vim.keymap.set('v', '>', '>gv', opts)
