f1661476a311e135aeb2bb120fdd48d91eeb81b8
Daniel Velasquez <danivelas4@gmail.com>
2024-12-25 15:22:19 -0500

    switched defaults M .config/nvim/lua/config/telescope.lua

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,22 +1,33 @@
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<C-h>", builtin.find_files, {})
-vim.keymap.set("n", "<leader>rr", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>tb", builtin.buffers, {})
-
--- <leader>ta: Search all file names, including gitignored and hidden files
-vim.keymap.set("n", "<leader>ta", function()
+-- search all file names, including gitignored and hidden files
+vim.keymap.set("n", "<C-h>", function()
   builtin.find_files({
     hidden = true,
     no_ignore = true,
   })
 end, {})
 
--- <leader>ra: Grep all files, including gitignored and hidden files
-vim.keymap.set("n", "<leader>ra", function()
+-- grep all files, including gitignored and hidden files
+vim.keymap.set("n", "<leader>rr", function()
   builtin.live_grep({
     additional_args = function()
       return { "--hidden", "--no-ignore" }
     end,
   })
 end, {})
+
+-- telescope mini: search all files names not including gitignored and hidden files
+vim.keymap.set("n", "<leader>tm", function()
+  builtin.find_files({
+    hidden = false,
+    no_ignore = false,
+  })
+end, {})
+
+
+-- grep mini: search all files names not including gitignored and hidden files
+vim.keymap.set("n", "<leader>rm", builtin.live_grep, {})
+
+-- telescope on buffers
+vim.keymap.set("n", "<leader>tb", builtin.buffers, {})
