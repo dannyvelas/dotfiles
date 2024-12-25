cc5270292477d669da954bf49e18a585e2ab8e42
Daniel Velasquez <danivelas4@gmail.com>
2024-12-25 15:16:15 -0500

    improved telescope bindings M .config/nvim/lua/config/telescope.lua

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,5 +1,22 @@
-builtin = require("telescope.builtin")
-telescope = require('telescope')
+local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<C-h>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>rr", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>tb", builtin.buffers, {})
+
+-- <leader>ta: Search all file names, including gitignored and hidden files
+vim.keymap.set("n", "<leader>ta", function()
+  builtin.find_files({
+    hidden = true,
+    no_ignore = true,
+  })
+end, {})
+
+-- <leader>ra: Grep all files, including gitignored and hidden files
+vim.keymap.set("n", "<leader>ra", function()
+  builtin.live_grep({
+    additional_args = function()
+      return { "--hidden", "--no-ignore" }
+    end,
+  })
+end, {})
