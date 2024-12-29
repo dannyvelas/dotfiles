dd91f4a952a4293be785e1ca03626be057664eb0
Daniel Velasquez <danivelas4@gmail.com>
2024-12-29 16:56:43 -0500

    fixed bug where Nvim, in command mode, if there is a completion menu, NVIM won't register that I moved my highlight to the second option and it will just execute the first option M .config/nvim/lua/config/nvim-cmp.lua

--- .config/nvim/lua/config/nvim-cmp.lua
+++ .config/nvim/lua/config/nvim-cmp.lua
@@ -21,17 +21,7 @@ cmp.setup({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       -- Use <CR>(Enter) to confirm selection
-      ["<CR>"] = cmp.mapping({
-         i = function(fallback)
-           if cmp.visible() and cmp.get_active_entry() then
-             cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-           else
-             fallback()
-           end
-         end,
-         s = cmp.mapping.confirm({ select = true }),
-         c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-       }),
+      ['<CR>'] = cmp.mapping.confirm({ select = false }),
 
       -- A super tab
       -- source: https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
