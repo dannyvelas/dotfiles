586a60066cf866e3012757d84c095dac43f4de1b
Daniel Velasquez <danivelas4@gmail.com>
2025-01-08 23:23:03 -0500

    got it to work now M .config/nvim/snippets/markdown.lua

--- .config/nvim/snippets/markdown.lua
+++ .config/nvim/snippets/markdown.lua
@@ -1,39 +1,34 @@
 local ls = require("luasnip")
-local sn = ls.snippet_node
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 local d = ls.dynamic_node
+local sn = ls.snippet_node
 
 -- Function to get the current filename without the directory or extension
-local function file_name()
+local function get_filename()
   local full_path = vim.fn.expand("%:t") -- Get the current file's name
   return full_path:gsub("%.%w+$", "")   -- Remove the extension
-end
-
--- Dynamic node to set the title to the current file name
---local function dynamic_title(args, snip)
---  return sn(nil, {
---    i(1, file_name()), -- Default value is the file name, editable
---  })
---end
-
-local function date_today()
-  return os.date("%Y-%m-%d")
 end
 
 ls.add_snippets("markdown", {
   s("fm", {
     t("---"),
     t({ "", "title: '" }),
-    d(1, function() -- Dynamic node to pre-fill the title with the filename
-      return sn(nil, { i(1, file_name()) }) -- Default is the filename, editable
+    -- Use a dynamic node for the title field
+    d(1, function()
+      local fname = get_filename()
+      return sn(nil, { i(1, fname) }) -- Pre-fill with filename
     end),
     t("'"),
-    t({ "", "date: " }), f(date_today), -- Dynamically insert today's date
-    t({ "", "publish: " }), i(2, "false"),
+    t({ "", "date: " }),
+    f(function() return os.date("%Y-%m-%d") end), -- Today's date
+    t({ "", "publish: " }),
+    i(2, "false"), -- Default to 'false', editable
     t({ "", "---", "" }),
-    t("Tags: [["), i(0), t("]]"),
+    t("Tags: ["),
+    i(0), -- Final jump target
+    t("]"),
   })
 })
