1170c444175bb9422e42e572c3ef3d9ef202addf
Daniel Velasquez <danivelas4@gmail.com>
2025-02-23 09:51:02 -0500

    added "<leader>td" mapping to fuzzy search a directory and then fuzzy search a file within that directory M .config/nvim/lua/config/telescope.lua

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,5 +1,7 @@
 -- load telescope
 local builtin = require("telescope.builtin")
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 
 -- make telescope use fzf
 require('telescope').setup {
@@ -32,10 +34,25 @@ end, { noremap = true, silent = true })
 
 -- find files in different directory
 vim.keymap.set("n", "<leader>td", function()
+  -- Use Telescope to pick a directory first
   builtin.find_files({
-    cwd = vim.fn.input("Directory: ", "", "dir"),
-    hidden = false,
-    no_ignore = false,
+    prompt_title = "Select Directory",
+    cwd = "/Users/dannyvelasquez",
+    find_command = { "fd", "--type", "d", "--hidden", "--exclude", ".git", "--no-ignore" }, -- Only search for directories
+    attach_mappings = function(prompt_bufnr, map)
+      local function enter()
+        local selection = action_state.get_selected_entry()
+        if selection then
+          local dir = selection[1] -- Get the selected directory
+          actions.close(prompt_bufnr)
+          -- Now open find_files inside the selected directory
+          builtin.find_files({ cwd = dir })
+        end
+      end
+      map("i", "<CR>", enter)
+      map("n", "<CR>", enter)
+      return true
+    end,
   })
 end, { noremap = true, silent = true })
 
