3127fea4d6f67f409426f141c72f408503329462
Daniel Velasquez <danivelas4@gmail.com>
2025-05-27 18:57:19 -0400

    M .config/nvim/lazy-lock.json M .config/nvim/lua/plugins/plugins.lua

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -10,6 +10,22 @@ return {
   -- my plugins
   "tpope/vim-fugitive",
   "tpope/vim-rsi",
+  -- edit stuff
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      -- add a keymap to complete for <c-x><c-f>
+      {
+        "<C-x><C-f>",
+        mode = { "i" },
+        function()
+          require("fzf-lua").complete_path()
+        end,
+        desc = "Fuzzy complete file",
+        silent = true,
+      },
+    },
+  },
   -- disable stuff
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
