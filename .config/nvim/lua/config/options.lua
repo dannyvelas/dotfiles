e61163ac2c6f5a3a86814309873cf42ea5d8f378
Daniel Velasquez <danivelas4@gmail.com>
2025-06-27 14:12:37 -0400

    added status line to each window. also updated plugins M .config/nvim/lazy-lock.json M .config/nvim/lua/config/options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,3 +4,4 @@
 vim.opt.hlsearch = false -- do not highlight matches
 vim.opt.timeoutlen = 1000 -- make timeoutlen 1 second so that it's easier to press <c-x><c-a> in command line mode
 vim.opt.showmode = true -- not using lualine so i need this
+vim.opt.laststatus = 2 -- not using lualine so i want one status line per window
