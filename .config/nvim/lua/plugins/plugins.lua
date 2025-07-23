d3e353cfac6dfba4acd81c7f3f662c97aeec2b32
Daniel Velasquez <danivelas4@gmail.com>
2025-07-22 23:50:22 -0400

    finally read documentation of lua snip and realized i had to add a package.json file M .config/nvim/lazyvim.json M .config/nvim/lua/plugins/plugins.lua A .config/nvim/snippets/markdown.json D .config/nvim/snippets/markdown.lua A .config/nvim/snippets/package.json

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -20,27 +20,7 @@ return {
   -- configure stuff
   {
     "saghen/blink.cmp",
-    dependencies = {
-      {
-        "L3MON4D3/LuaSnip",
-        build = "make install_jsregexp", -- needed for VSCode style variable transforms
-        version = "v2.*",
-        dependencies = {
-          {
-            "rafamadriz/friendly-snippets",
-          },
-        },
-        config = function()
-          require("luasnip").setup({ enable_autosnippets = true })
-          require("luasnip.loaders.from_vscode").lazy_load()
-          require("luasnip.loaders.from_lua").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
-        end,
-      },
-    },
     opts = {
-      snippets = {
-        preset = "luasnip",
-      },
       completion = {
         list = {
           selection = {
