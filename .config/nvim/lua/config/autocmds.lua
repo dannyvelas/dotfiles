de2856f128b42c1df028f8ddd1d772a466f05d31
Daniel Velasquez <danivelas4@gmail.com>
2025-07-25 10:55:39 -0400

    disabling autoformat for json M .config/nvim/lua/config/autocmds.lua

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -26,3 +26,10 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.breakindentopt = "shift:2,min:40"
   end,
 })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "json" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
